a7851ff78963f5cfbc87c690ab46b0fe9b2cbc7c
moseskamira <moses.african@gmail.com>
2025-07-07 23:21:24 +0300

    Improve Image Design

---
+++ lib/presentation/pages/homepage/widgets/breed_widget.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+import '../../breedimages/breed_images_page.dart';
+
+class BreedWidget extends StatelessWidget {
+  final String breed;
+
+  const BreedWidget({super.key, required this.breed});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (_) => BreedImagesPage(breedName: breed),
+          ),
+        );
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(4),
+          border: Border.all(width: 1, color: Colors.blueGrey),
+        ),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              breed[0].toUpperCase() + breed.substring(1),
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
