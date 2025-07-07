b6e61dc6e6bc2bf58f8c1e8e0f40ae9349dc18e0
moseskamira <moses.african@gmail.com>
2025-07-07 19:33:16 +0300

    Create develop branch

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -7,8 +7,6 @@ import 'home_page.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
@@ -25,7 +23,7 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: const MyHomePage(title: 'Flutter Demo Home Page'),
+        home: const MyHomePage(title: 'Dog Breeds'),
       ),
     );
   }
